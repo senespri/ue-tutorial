806daaa08ce162946adc0b9fca7b2bc87054a58d
Marcin Michalski <marcin.michalski86@gmail.com>
2024-05-23 00:10:16 +0200

    Lecture 10 - Part 2

---
+++ Source/UE5Tutorial/Private/AI/SAIController.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Called when the game starts or when spawned
+void ASAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	RunBehaviorTree(BehaviorTree);
+	if (BehaviorTree == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Missing behaviour tree"));
+	}
+
+	if (APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0))
+	{
+		if (UBlackboardComponent* blackboard = GetBlackboardComponent())
+		{
+			blackboard->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
+
+			blackboard->SetValueAsObject("TargetActor", MyPawn);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Missing blackboard"));
+		}
+	}
+}
